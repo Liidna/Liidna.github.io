0cd66b2553f38604e4638b37b2d58b5d2a886035
lsticere <lsticere@gmail.com>
2024-05-17 22:02:56 +0300

    Smooth scroll

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,16 +4,24 @@ import { getConfigData } from "../data/configReader";
 import Profile from "../components/Profile";
 
 export default function About() {
-  const configData = getConfigData();
-  return (
-    <>
-      <Profile />
-      <div className="flex flex-col items-center justify-center py-8">
-        <h1 className="text-3xl font-semibold"><a href="https://www.printful.com/" className="text-[#9d105e] font-bold">Printful</a> Junior Software Tester</h1>
-        <p className="text-md font-normal text-gray-500 text-center px-3">
-          2023 - pašlaik
-        </p>
-      </div>
-    </>
-  );
+	const configData = getConfigData();
+	return (
+		<>
+			<Profile />
+			<div className="flex flex-col items-center justify-center py-8">
+				<h1 className="text-3xl font-semibold">
+					<a
+						href="https://www.printful.com/"
+						className="text-[#9d105e] font-bold"
+					>
+						Printful
+					</a>{" "}
+					Junior Software Tester
+				</h1>
+				<p className="text-md font-normal text-gray-500 text-center px-3">
+					2023 - pašlaik
+				</p>
+			</div>
+		</>
+	);
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -8,24 +8,34 @@ export default function Home() {
 
 	return (
 		<>
-    <div className="text-gray-800">
-			<div className="flex flex-col items-center justify-center py-8">
-				<h1 className="text-3xl font-bold">
-					{" "}
-					Maldinošs Dizains
-				</h1>
-			</div>
-			<div className="text-xl px-7 py-5 flex flex-col text-md font-normal leading-loose">
-				<p>{configData.deceptiveDesignIntro}</p>
-				<br />
-				<p>{configData.deceptiveDesignIntro2}</p>
+			<div className="text-gray-800">
+				<div className="flex flex-col items-center justify-center py-8">
+					<h1 className="text-3xl font-bold"> Maldinošs Dizains</h1>
+				</div>
 
-				<div className="flex flex-col items-center justify-center py-8 text-gray-300">
-					<FaAngleDown size={50}/>
+				<div className="text-xl px-7 py-5 flex flex-col text-md font-normal leading-loose">
+					<p>{configData.deceptiveDesignIntro}</p>
+					<br />
+					<p>{configData.deceptiveDesignIntro2}</p>
+					<br />
+					<div className="flex flex-col items-center justify-center py-32 text-gray-300">
+						<a href="#types" className=" hover:text-gray-900">
+							<FaAngleDown href="#types" size={50} />
+						</a>
+					</div>
+					<br />
 				</div>
 			</div>
-      </div>
-			<Projects />
+
+			<div>
+				<a id="types">
+        <div className="flex flex-col items-center justify-center py-8">
+					<h2 className="text-2xl font-semibold"> Maldinoša dizaina veidi</h2>
+				</div>
+				</a>
+				<Projects />
+				<Projects />
+			</div>
 		</>
 	);
 }
