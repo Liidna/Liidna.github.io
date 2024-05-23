4d23bd42a9b4e64a9004bacb9d9e08e0347a5e63
lsticere <lsticere@gmail.com>
2024-05-23 11:55:38 +0300

    Pārbaudījuma skats, nomainīti routes

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,29 +1,21 @@
-import Projects from "../components/Types";
 import { getConfigData } from "../data/configReader";
 
-import { FaAngleDown } from "react-icons/fa6";
-
-export default function Home() {
+export default function Test() {
 	const configData = getConfigData();
 
 	return (
 		<>
 			<div className="text-gray-800">
-				<div className="flex flex-col items-center justify-center py-8">
-					<h1 className="text-4xl font-bold"> Maldinošs Dizains</h1>
+				<div className="flex items-center justify-center py-8 gap-x-3">
+					<div className="w-4 h-2 bg-[#9d105e] rounded-full"></div>
+					<h1 className="text-4xl font-bold">Viktorīna</h1>
+					<div className="w-4 h-2 bg-[#9d105e] rounded-full"></div>
 				</div>
 
 				<div className="text-xl px-7 py-5 flex flex-col text-md font-normal leading-loose">
 					<p>{configData.deceptiveDesignIntro}</p>
 					<br />
 					<p>{configData.deceptiveDesignIntro2}</p>
-					<br />
-					<div className="flex flex-col items-center justify-center py-32 text-gray-300">
-						<a href="#types" className=" hover:text-gray-900">
-							<FaAngleDown href="#types" size={50} />
-						</a>
-					</div>
-					<br />
 				</div>
 			</div>
 
@@ -33,8 +25,6 @@ export default function Home() {
 						<h2 className="text-2xl font-semibold"> Maldinoša dizaina veidi</h2>
 					</div>
 				</a>
-				<Projects />
-				<Projects />
 			</div>
 		</>
 	);
--- src/routes/SiteRoutes.jsx
+++ src/routes/SiteRoutes.jsx
@@ -1,8 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import About from "../pages/About";
-import Projects from "../pages/Projects";
-import Products from "../pages/Products";
+import Test from "../pages/Test";
 import PageNotFound from "../pages/PageNotFound";
 
 export default function SiteRoutes() {
@@ -10,8 +9,7 @@ export default function SiteRoutes() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
-      <Route path="/projects" element={<Projects />} />
-      <Route path="/products" element={<Products />} />
+      <Route path="/test" element={<Test />} />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
