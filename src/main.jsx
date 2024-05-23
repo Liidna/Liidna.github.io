4941ee264552fe70ba8d8a1f0654673cfbbc1042
lsticere <lsticere@gmail.com>
2024-05-23 14:42:50 +0300

    Failo github pages, labojumi

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
@@ -8,5 +7,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <App />
   </BrowserRouter>
-  ,
 )
